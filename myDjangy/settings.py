3ff02d7d6ee63d7480af0cc2635596900b7b1b69
Sayed Riaydh <riaydh06@gmail.com>
2024-05-24 12:59:44 +0600

    feat: mysql setup

--- myDjangy/settings.py
+++ myDjangy/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'myDjangy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '12345678'
     }
 }
 
